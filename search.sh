b5532f7803d4cdc2042d58eb8ff4a1b74e4e5f17
brijhs <brijhs@gmail.com>
2023-02-09 20:55:06 -0500

    Reverse sort only for lift

--- search.sh
+++ search.sh
@@ -2,8 +2,14 @@
 
 #Takes file to be searched as argument 1 and search term as argument 2
 #and the column to be sorted by (determined by new.search()) as argument 3
-cat $1 | head -n1 > "$2".csv && cat $1 | grep -i "$2" >> temp.csv
-cat temp.csv | sort -t, -n -k$3 -r >> "$2".csv
+if  [ "$1" ==  'lift.csv' ]
+then
+	cat $1 | head -n1 > "$2".csv && cat $1 | grep -i "$2" >> temp.csv
+	cat temp.csv | sort -t, -n -k$3 -r >> "$2".csv
+else
+	cat $1 | head -n1 > "$2".csv && cat $1 | grep -i "$2" >> temp.csv
+	cat temp.csv | sort -t, -n -k$3 >> "$2".csv
+fi
 rm -f temp.csv
 open "$2".csv
 sleep 1
